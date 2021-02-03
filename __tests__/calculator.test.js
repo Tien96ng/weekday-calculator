2dabd9ec9f31443d2539ebdd63bb3a786c9e5eea
tien96ng <tien96ng@ghmail.com>
2021-02-03 10:36:13 -0800

    Pass initial testing for validDateChecker()

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,15 +1,17 @@
-import weekdayCalculator from "../src/js/calculator.js";
+import { getDate, validDateChecker } from "../src/js/calculator.js";
 
-describe("weekdayCalculator", () => {
-
-
+describe("getDate", () => {
   test("Should return day of the given date.", () => {
-    expect(weekdayCalculator("01/01/2021")).toEqual("Fri");
+    expect(getDate("01/01/2021")).toEqual("Fri");
   });
 
   test("Should return day of the given date in the future.", () => {
-    expect(weekdayCalculator("01/01/2022")).toEqual("Sat");
+    expect(getDate("01/01/2022")).toEqual("Sat");
   });
+});
 
-
+describe("validDateChecker", () => {
+  test("Should console.log to tell the user if the date entered is valid.", () => {
+    expect(validDateChecker("Andrew")).toEqual("Please Enter in a valid Date!");
+  });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,4 +1,13 @@
-export default function getDate(input) {
+export function getDate(input) {
   let inputDate = new Date(Date.parse(input));
   return inputDate.toString().slice(0, 3);
+}
+
+export function validDateChecker(input) {
+  let inputDate = new Date(Date.parse(input)); // Andrew = NaN
+  if(isNaN(inputDate.getHours())) {
+    return "Please Enter in a valid Date!";
+  } else {
+    return "Date is Valid.";
+  }
 }
