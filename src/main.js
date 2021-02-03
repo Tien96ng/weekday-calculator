3f7fccda87bab565a57a702a726158ba8b3bddf2
tien96ng <tien96ng@ghmail.com>
2021-02-03 11:01:55 -0800

    Finish UI with jQuery.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { getDate, validDateChecker } from "./js/calculator.js";
+
+$(document).ready(() => {
+  $("#form").submit((e) => {
+    e.preventDefault();
+    let date = $("#date").val();
+
+    let checkedDate =  validDateChecker(date);
+    if(!checkedDate) { // checkedDate === false
+      $("#invalid-message").show();
+    } else {
+      let validDate = getDate(date);
+      $("#day").text(`Day on date entered: ${validDate}`);
+      // "Day on date entered: " + validDate
+    }
+
+    $("#form")[0].reset();
+  });
+});
