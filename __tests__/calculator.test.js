a3b07f6d5742e45e7984e996c698652f6f07591e
tien96ng <tien96ng@ghmail.com>
2021-02-03 10:13:36 -0800

    Fail test2 for future date.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,5 +7,9 @@ describe("weekdayCalculator", () => {
     expect(weekdayCalculator("01/01/2021")).toEqual("Fri");
   });
 
+  test("Should return day of the given date in the future.", () => {
+    expect(weekdayCalculator("01/01/2022")).toEqual("Fri");
+  });
+
 
 });
